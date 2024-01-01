376409d4de05d9208df57babb1db755d57d8ae35
Aleksandr <a.folyak2000@gmail.com>
2024-01-01 18:59:52 +0600

    add tableView and CarsCell

--- AVBY/AVBY/Controllers/Auto/AutoController.swift
+++ AVBY/AVBY/Controllers/Auto/AutoController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
-class AutoController: BaseController {
+final class AutoController: BaseController {
+    
+    // MARK: private properties
+    private let tableView = UITableView()
+    
+    // MARK: Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -8,5 +13,38 @@ class AutoController: BaseController {
         navigationController?.tabBarItem.title = Resources.Titles.TabBar.auto
         
         addNavBarButton()
+        configureTableView()
     }
+    
+    // MARK: - API
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        ])
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CarsCell.self, forCellReuseIdentifier: "CarsCell")
+    }
+}
+
+// MARK: - extension
+extension AutoController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CarsCell", for: indexPath) as? CarsCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    
 }
---
+++ AVBY/AVBY/Controllers/Cells/CarsCell.swift
@@ -0,0 +1,23 @@
+//
+//  CarsCell.swift
+//  AVBY
+//
+//  Created by Александр Фоляк on 01.01.2024.
+//
+
+import UIKit
+
+class CarsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
